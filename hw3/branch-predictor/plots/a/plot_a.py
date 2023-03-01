9e78e530065eb21e6314e0cee6358f5efe639dcc
Matheus Farias <matheusfarias@g.harvard.edu>
2023-03-01 13:21:27 -0500

    missing perf

---
+++ hw3/branch-predictor/plots/a/plot_a.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+# deal 1 bit vs 2 bit
+deal_naive = [76.78, 77.96, 82.01, 82.20, 82.30, 82.35, 82.37]
+deal_2bit = [84.14, 85.23, 87.44, 87.62, 87.71, 87.79, 87.81]
+
+# quantum 1 bit vs 2 bit
+quantum_naive = [91.22, 91.22, 91.22, 91.22, 91.22, 91.22, 91.22]
+quantum_2bit = [93.24, 93.24, 93.24, 93.25, 93.25, 93.25, 93.25]
+
+size = [128,256, 512, 1024, 2048, 4096, 8192]
+
+fig, ax = plt.subplots()
+plt.semilogx(size, deal_naive, marker = 'o', ms=5, lw=2, label = '1-bit', color = 'crimson')
+plt.semilogx(size, deal_2bit, marker = 's', ms=5, lw=2, label = '2-bit', color='navy')
+plt.xscale('log',base=2)
+plt.xlabel(r'Branch Prediction Buffer size')
+plt.ylabel(r'Accuracy')
+ax = plt.gca()
+ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
+plt.title('1-bit vs 2-bit sweeping branch prediction buffer size (deal)')
+plt.legend()
+plt.savefig('a_deal.pdf')
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+plt.semilogx(size, quantum_naive, marker = 'o', ms=5, lw=2, label = '1-bit', color = 'crimson')
+plt.semilogx(size, quantum_2bit, marker = 's', ms=5, lw=2, label = '2-bit', color='navy')
+plt.xscale('log',base=2)
+plt.xlabel(r'Branch Prediction Buffer size')
+plt.ylabel(r'Accuracy')
+ax = plt.gca()
+ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
+plt.title('1-bit vs 2-bit sweeping branch prediction buffer size (quantum)')
+plt.legend()
+plt.savefig('a_quantum.pdf')
+plt.show()
+
---
+++ hw3/branch-predictor/plots/b/plot_b.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+# deal 1 bit vs 2 bit
+deal_12_2level = [96.51, 96.87, 97.08, 97.21, 97.27, 97.31]
+deal_14_2level = [96.76, 97.09, 97.28, 97.39, 97.45, 97.49]
+deal_16_2level = [96.97, 97.27, 97.44, 97.54, 97.61, 97.63]
+
+# quantum 1 bit vs 2 bit
+quantum_12_2level = [99.72, 99.72, 99.72, 99.72, 99.72, 99.72]
+quantum_14_2level = [99.75, 99.75, 99.75, 99.75, 99.75, 99.75]
+quantum_16_2level = [99.81, 99.81, 99.81, 99.81, 99.81, 99.81]
+
+size = [128,256, 512, 1024, 2048, 4096]
+
+fig, ax = plt.subplots()
+plt.semilogx(size, deal_12_2level, marker = 'o', ms=5, lw=2, label = '12 bits', color = 'crimson')
+plt.semilogx(size, deal_14_2level, marker = 's', ms=5, lw=2, label = '14 bits', color='navy')
+plt.semilogx(size, deal_16_2level, marker = '^', ms=5, lw=2, label = '16 bits', color='turquoise')
+plt.xscale('log',base=2)
+plt.xlabel(r'Pattern Table size')
+plt.ylabel(r'Accuracy')
+ax = plt.gca()
+ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
+plt.title('2-level predictor sweeping pattern table size (deal)')
+plt.legend()
+plt.savefig('b_deal.pdf')
+plt.show()
+plt.clf()
+
+fig, ax = plt.subplots()
+plt.semilogx(size, quantum_12_2level, marker = 'o', ms=5, lw=2, label = '12 bits', color = 'crimson')
+plt.semilogx(size, quantum_14_2level, marker = 's', ms=5, lw=2, label = '14 bits', color='navy')
+plt.semilogx(size, quantum_16_2level, marker = '^', ms=5, lw=2, label = '16 bits', color='turquoise')
+plt.xscale('log',base=2)
+plt.xlabel(r'Pattern Table size')
+plt.ylabel(r'Accuracy')
+ax = plt.gca()
+ax.xaxis.set_major_formatter(mticker.ScalarFormatter())
+plt.title('2-level predictor sweeping pattern table size (quantum)')
+plt.legend()
+plt.savefig('b_quantum.pdf')
+plt.show()
+
