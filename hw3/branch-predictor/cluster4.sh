1dc75fb427d873ebf96281fd27dd04fa3fa665b1
Matheus Farias <matheusfarias@g.harvard.edu>
2023-03-02 09:22:31 -0500

    last simulations

--- hw3/branch-predictor/cluster4.sh
+++ hw3/branch-predictor/cluster4.sh
@@ -5,7 +5,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='4096'
@@ -14,7 +14,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='4096'
@@ -23,7 +23,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='4096'
@@ -32,7 +32,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='4096'
@@ -41,7 +41,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_12_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 # 2 level 14
@@ -51,7 +51,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='16384'
@@ -60,7 +60,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='16384'
@@ -69,7 +69,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='16384'
@@ -78,7 +78,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='16384'
@@ -87,7 +87,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_14_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 # 2 level 16
@@ -97,7 +97,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_4 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='65536'
@@ -106,7 +106,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_8 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='65536'
@@ -115,7 +115,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_16 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='65536'
@@ -124,7 +124,7 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_32 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
 ht_size='65536'
@@ -133,6 +133,6 @@ sed -i "s/^#define HT_LENGTH.*/#define HT_LENGTH ${ht_size}/" ./hw3_b/hw3.cpp
 sed -i "s/^#define SIZE.*/#define SIZE ${pt_size}/" ./hw3_b/hw3.cpp
 cd ./hw3_b/
 make
-$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25
+$PIN_ROOT/pin -t obj-intel64/hw3.so -o quantum_b_16_64 -- $HOME/shared_data/benchmarks/libquantum_O3 400 25 &
 cd ..
 
