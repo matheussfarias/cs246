f9ed816121e19514c94f1e9a2fd501ca6cbb88ed
Matheus Farias <matheusfarias@g.harvard.edu>
2023-03-08 12:38:53 -0500

    roofline memory added

--- section_roofline/flops/run.sh
+++ section_roofline/flops/run.sh
@@ -18,8 +18,8 @@ export OMP_NUM_THREADS=$NUM_CORES
 # N:total_cache:num_threads - N:360kB:32 - 360kB total memory size, 36 threads, each core gets 10kB
 
 # Note: add _sp after peakflops to get single precision results
-likwid-bench -t peakflops -W N:320kB:32
-likwid-bench -t peakflops_sse -W N:320kB:32
-likwid-bench -t peakflops_avx -W N:320kB:32
-likwid-bench -t peakflops_avx_fma -W N:320kB:32
+likwid-bench -t peakflops_sp -W N:320kB:32
+likwid-bench -t peakflops_sp_sse -W N:320kB:32
+likwid-bench -t peakflops_sp_avx -W N:320kB:32
+likwid-bench -t peakflops_sp_avx_fma -W N:320kB:32
 
