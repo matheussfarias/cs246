3c02558d43d976877c4f1cc1215e45a43503a5c8
Matheus Farias <matheusfarias@g.harvard.edu>
2023-03-08 12:23:54 -0500

    roofline

--- section_roofline/flops/run.sh
+++ section_roofline/flops/run.sh
@@ -15,11 +15,11 @@ export OMP_NUM_THREADS=$NUM_CORES
 
 
 # Modify script so each core gets 10kB of data
-# N:total_cache:num_threads - N:360kB:36 - 360kB total memory size, 36 threads, each core gets 10kB
+# N:total_cache:num_threads - N:360kB:32 - 360kB total memory size, 36 threads, each core gets 10kB
 
 # Note: add _sp after peakflops to get single precision results
-#likwid-bench -t peakflops -W N:360kB:36
-#likwid-bench -t peakflops_sse -W N:360kB:36
-#likwid-bench -t peakflops_avx -W N:360kB:36
-#likwid-bench -t peakflops_avx_fma -W N:360kB:36
+likwid-bench -t peakflops -W N:320kB:32
+likwid-bench -t peakflops_sse -W N:320kB:32
+likwid-bench -t peakflops_avx -W N:320kB:32
+likwid-bench -t peakflops_avx_fma -W N:320kB:32
 
