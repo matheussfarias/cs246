309b97002c42db1c182314eb130f6d74e5a17d98
Matheus Farias <matheusfarias@g.harvard.edu>
2023-03-22 19:28:28 -0400

    swap implemented?

--- hw4/code_1_7/hw4.cpp
+++ hw4/code_1_7/hw4.cpp
@@ -590,7 +590,7 @@ void CreateCaches(void)
                 break;
             case 2:
                 parser >> bsize >> comma >> csize >> comma >> assoc >> comma >> vsize;
-                vcache = new victim_cache(bsize, csize);
+                vcache = new victim_cache(bsize, 8*bsize);
                 dcache = new l1dcache(bsize, csize, assoc, llcache, vcache);
                 break;
             default:
